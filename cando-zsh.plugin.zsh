729f7b6c9e3027c3fe1f8aedff4d5454973c4579
Dane Grant <danecando@gmail.com>
2015-06-24 00:26:19 -0400

    gussied up

--- cando-zsh.plugin.zsh
+++ cando-zsh.plugin.zsh
@@ -1,4 +1,15 @@
+# sudo for aliases
 alias sudo 'sudo '
-alias b2d='boot2docker up && $(boot2docker shellinit)'
-alias zshrc='vim ~./zshrc'
+
+# hosts file
 alias hosts='sudo vim /private/etc/hosts'
+
+# zsh
+alias zshrc='vim ~./zshrc'
+
+# boot2docker
+alias b2d='boot2docker'
+alias b2dset='eval $(boot2docker shellinit)'
+alias b2dlaunch='b2d up && b2dset'
+alias b2dreset='boot2docker stop && boot2docker delete'
+alias b2dweb='open http://$(boot2docker ip)'
